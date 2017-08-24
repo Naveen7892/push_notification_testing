8ee23ee9ca97076ceb7306654f732b43f8b64410
Naveen Kumar <metanaveen@outlook.com>
2017-08-24 19:35:01 +0530

    Testing Onesignal

--- www/js/app.js
+++ www/js/app.js
@@ -18,4 +18,25 @@ angular.module('starter', ['ionic'])
       push.saveToken(token);  // persist the token in the Ionic Platform
     });
   });
+
+  // Push notification using OneSignal
+  ionic.Platform.ready(function() {
+    // Enable to debug issues.
+    // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+    
+    var notificationOpenedCallback = function(jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+
+    window.plugins.OneSignal
+      .startInit("1963cb3b-f80f-4bc5-b202-3b1d25c34b02")
+      .handleNotificationOpened(notificationOpenedCallback)
+      .endInit();
+      
+    // Call syncHashedEmail anywhere in your app if you have the user's email.
+    // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+    // window.plugins.OneSignal.syncHashedEmail(userEmail);
+  })
+
+
 })
